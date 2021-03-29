3dc64b237ddf07bac31a00d710cd7fc66c1155d7
mattbmoorin <tzaratape@gmail.com>
2021-03-28 20:26:53 -0500

    started fetch implementation

--- ecmasynth_backend/db/migrate/20210328060057_create_synths.rb
+++ ecmasynth_backend/db/migrate/20210328060057_create_synths.rb
@@ -1,7 +1,9 @@
 class CreateSynths < ActiveRecord::Migration[6.0]
   def change
     create_table :synths do |t|
-
+      t.integer :envelope
+      t.integer :reverb
+      t.integer :delay
       t.timestamps
     end
   end
