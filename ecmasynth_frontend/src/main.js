c50775a589d1172846357164b39ff7cdf2911b20
mattbmoorin <tzaratape@gmail.com>
2021-05-31 21:07:12 -0500

    s;

--- ecmasynth_frontend/src/main.js
+++ ecmasynth_frontend/src/main.js
@@ -4,25 +4,25 @@ envelopeButton.addEventListener('click', setEnvelope);
 delayButton.addEventListener('click', setDelay);
 reverbButton.addEventListener('click', setReverb);
 synthForm.addEventListener('submit', createSynth);
-
+g;
 function darkMode() {
   document.body.classList.toggle('dark-mode');
-};
+}
 
 function setEnvelope(e) {
   envelopeGenerator = envelopeValue.value;
   console.log(envelopeGenerator);
-};
+}
 
 function setReverb(e) {
   reverbSetting.decay = reverbValue.value;
   console.log(reverbSetting.decay);
-};
+}
 
 function setDelay(e) {
   delaySetting.delayTime.value = delayValue.value;
   console.log(delaySetting.delayTime.value);
-};
+}
 
 function createSynth(e) {
   e.preventDefault();
@@ -30,45 +30,47 @@ function createSynth(e) {
   let synth = {
     envelope: envelope.value,
     reverb: reverb.value,
-    delay: delay.value
+    delay: delay.value,
   };
   return fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      },
-      body: JSON.stringify(synth)
-    })
-    .then(res => res.json())
-    .then(res => getSingleSynth(res));
-};
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+    body: JSON.stringify(synth),
+  })
+    .then((res) => res.json())
+    .then((res) => getSingleSynth(res));
+}
 
 function getSingleSynth(res) {
-  return fetch(baseUrl + res.id).then(res => res.json())
-    .then(res => appendProcess(res))
-    .then(res => appendSinglePreset(res))
-    .catch(error => console.log(error));
-};
+  return fetch(baseUrl + res.id)
+    .then((res) => res.json())
+    .then((res) => appendProcess(res))
+    .then((res) => appendSinglePreset(res))
+    .catch((error) => console.log(error));
+}
 
 function getSynths() {
-  return fetch(baseUrl).then(res => res.json())
-    .then(res => appendPresets(res))
-    .catch(error => console.log(error));
-};
+  return fetch(baseUrl)
+    .then((res) => res.json())
+    .then((res) => appendPresets(res))
+    .catch((error) => console.log(error));
+}
 
 function appendPresets(res) {
   console.log(res);
 
-  let presetElements = (res).map(preset => appendProcess(preset));
-  presetElements.forEach(preset => presetsContainer.append(preset));
-};
+  let presetElements = res.map((preset) => appendProcess(preset));
+  presetElements.forEach((preset) => presetsContainer.append(preset));
+}
 
 function appendSinglePreset(res) {
   console.log(res);
 
   presetsContainer.append(res);
-};
+}
 
 function appendProcess(preset) {
   let loadButton = document.createElement('button');
@@ -78,9 +80,8 @@ function appendProcess(preset) {
   loadButton.innerHTML = 'Load Preset';
   deleteButton.innerHTML = 'Delete Preset';
 
-  loadButton.className = 'button is-info'
-  deleteButton.className = 'button is-danger is-outlined'
-
+  loadButton.className = 'button is-info';
+  deleteButton.className = 'button is-danger is-outlined';
 
   ul.innerHTML = `User Preset ${preset.id}<br />`;
   ul.id = preset['id'];
@@ -97,12 +98,12 @@ function appendProcess(preset) {
   deleteButton.addEventListener('click', (e) => {
     e.target.parentNode.remove();
     return fetch(baseUrl + preset.id, {
-        method: 'DELETE',
-      })
-      .then(res => res.json())
-      .then(res => console.log(res));
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   });
   return ul;
-};
+}
 
 getSynths();
